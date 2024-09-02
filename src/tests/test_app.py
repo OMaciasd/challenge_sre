7e1a188a75aa7079e9dc2c2fe9ed8cda7792936b
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-09-02 16:33:10 -0500

    Update test_app.py

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -6,10 +6,8 @@ class FlaskAppTests(unittest.TestCase):
     
     @patch('app.app.initialize_rabbitmq')
     def test_homepage(self, mock_initialize_rabbitmq):
-        # Configura el mock para devolver valores predeterminados
         mock_initialize_rabbitmq.return_value = (None, None)
 
-        # Crea un cliente de prueba para la aplicación Flask
         with app.test_client() as client:
             response = client.get('/')
             assert response.status_code == 200
