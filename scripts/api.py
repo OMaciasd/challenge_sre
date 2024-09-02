8d238fe9e5db6a5b61867f960042e545bce75c0f
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-09-02 10:58:36 -0500

    Update api.py

--- scripts/api.py
+++ scripts/api.py
@@ -10,7 +10,7 @@ url = os.getenv("API_URL")
 with open('data.json', 'r') as json_file:
     data = json.load(json_file)
 
-response = requests.post(url, json=data)
+response = requests.post(url, json=data, timeout=10)
 
 print(response.status_code)
 print(response.json())
