d379e429ee1b7d4ad7cef42956b4dba1fba3a32c
Oscar Andres Macias Narvaez <omaciasnarvaez@gmail.com>
2024-09-12 00:00:28 -0500

    ⚡ fix(github-actions) Change diagram

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my-python-project',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+    ],
+    python_requires='>=3.12',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
