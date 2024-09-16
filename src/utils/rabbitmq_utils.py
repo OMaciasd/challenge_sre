2bb2f16ab7ef63ffce3f5e809ce9e8924e784367
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-09-16 05:43:35 -0500

    Update rabbitmq_utils.py

--- src/utils/rabbitmq_utils.py
+++ src/utils/rabbitmq_utils.py
@@ -1,16 +1,27 @@
+import pika
 import logging
 from config.config import Config
 
 logger = logging.getLogger(__name__)
 
-
 def parse_rabbitmq_url():
     """
-    Analiza la URL de RabbitMQ desde la configuración.
+    Analyzes RabbitMQ URL from the configuration.
     """
     url = Config.RABBITMQ_URI
     if not url:
-        logger.error("RABBITMQ_URI no está configurado.")
-        raise ValueError("RABBITMQ_URI no está configurado.")
+        logger.error("RABBITMQ_URI is not configured.")
+        raise ValueError("RABBITMQ_URI is not configured.")
+
+    try:
+        connection_params = pika.ConnectionParameters(
+            host=url,
+            socket_timeout=10 
+        )
+        connection = pika.BlockingConnection(connection_params)
+        connection.close()
+    except Exception as e:
+        logger.error(f"Error connecting to RabbitMQ: {e}")
+        raise
 
     return url
