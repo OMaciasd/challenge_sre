1f59be35e632b0747148f463e6340e75cd57a536
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-09-16 05:33:11 -0500

    Update test_rabbitmq_ci.py

--- src/tests/ci/test_rabbitmq_ci.py
+++ src/tests/ci/test_rabbitmq_ci.py
@@ -7,7 +7,7 @@ from utils.secrets_utils import validate_secrets
 
 @pytest.fixture(autouse=True)
 def mock_rabbitmq_uri():
-    with patch.dict(os.environ, {'RABBITMQ_URI': 'amqp://guest:guest@localhost:5672/'}):
+    with patch.dict(os.environ, {'RABBITMQ_URI': 'mocked_rabbitmq_url'}):
         yield
 
 @pytest.mark.timeout(5)
@@ -18,17 +18,17 @@ def test_validate_secrets_all_vars_set():
         pytest.fail("validate_secrets raised ValueError unexpectedly!")
 
 @pytest.mark.timeout(5)
-@patch('pika.BlockingConnection')
+@patch('utils.rabbitmq_utils.pika.BlockingConnection')
 def test_parse_rabbitmq_url(mock_blocking_connection):
-    mock_connection = Mock()
-    mock_channel = Mock()
+    mock_connection = MagicMock()
+    mock_channel = MagicMock()
     mock_connection.channel.return_value = mock_channel
     mock_blocking_connection.return_value = mock_connection
 
     result = parse_rabbitmq_url()
 
     mock_blocking_connection.assert_called_once()
-    
-    expected_url = os.getenv('RABBITMQ_URI', 'amqp://guest:guest@localhost:5672/')
+
+    expected_url = os.getenv('RABBITMQ_URL', 'mocked_rabbitmq_url')
     if result != expected_url:
         pytest.fail(f"Expected '{expected_url}', but got {result}")
