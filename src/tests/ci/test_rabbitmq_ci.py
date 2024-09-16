7a5c6ab05cbf66f5289b98065834a35b9a7c4a9e
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-09-16 06:52:33 -0500

    Update test_rabbitmq_ci.py

--- src/tests/ci/test_rabbitmq_ci.py
+++ src/tests/ci/test_rabbitmq_ci.py
@@ -1,23 +1,23 @@
 import pytest
-from unittest.mock import patch
+import os
 from config.config import Config
 from utils.rabbitmq_utils import parse_rabbitmq_url
 from utils.secrets_utils import validate_secrets
 
+@pytest.fixture
+def set_rabbitmq_env_var():
+    os.environ['RABBITMQ_URI'] = Config.RABBITMQ_URI
+    yield os.environ['RABBITMQ_URI']
+    del os.environ['RABBITMQ_URI']
 
-@patch.object(Config, 'RABBITMQ_URI', 'amqp://guest:guest@localhost:5672/')
-def test_validate_secrets_all_vars_set():
+def test_validate_secrets_all_vars_set(set_rabbitmq_env_var):
     try:
         validate_secrets()
     except ValueError:
         pytest.fail("validate_secrets raised ValueError unexpectedly!")
 
-
-@patch.object(Config, 'RABBITMQ_URI', 'amqp://guest:guest@localhost:5672/')
-def test_parse_rabbitmq_url():
+def test_parse_rabbitmq_url(set_rabbitmq_env_var):
     result = parse_rabbitmq_url()
-    if result != 'amqp://guest:guest@localhost:5672/':
-        pytest.fail(
-            "Expected 'amqp://guest:guest@localhost:5672/',"
-            f"but got {result}"
-        )
+    expected_url = set_rabbitmq_env_var
+    if result != expected_url:
+        pytest.fail(f"Expected '{expected_url}', but got '{result}'")
