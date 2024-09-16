6309b6d527b5404df051061f50a44e281d8628f4
Oscar Macias. <omaciasnarvaez@gmail.com>
2024-09-16 05:49:51 -0500

    Update test_rabbitmq_ci.py

--- src/tests/ci/test_rabbitmq_ci.py
+++ src/tests/ci/test_rabbitmq_ci.py
@@ -1,14 +1,14 @@
 import pytest
 import os
 from unittest.mock import patch, MagicMock
-from config.config import Config
 from utils.rabbitmq_utils import parse_rabbitmq_url
 from utils.secrets_utils import validate_secrets
 
 @pytest.fixture(autouse=True)
 def mock_rabbitmq_uri():
-    with patch.dict(os.environ, {'RABBITMQ_URI': 'mocked_rabbitmq_url'}):
-        yield
+    rabbitmq_uri = os.getenv('RABBITMQ_URI', 'default_value_if_not_set')
+    with patch.dict(os.environ, {'RABBITMQ_URI': rabbitmq_uri}):
+        yield rabbitmq_uri
 
 @pytest.mark.timeout(5)
 def test_validate_secrets_all_vars_set():
@@ -19,7 +19,7 @@ def test_validate_secrets_all_vars_set():
 
 @pytest.mark.timeout(5)
 @patch('utils.rabbitmq_utils.pika.BlockingConnection')
-def test_parse_rabbitmq_url(mock_blocking_connection):
+def test_parse_rabbitmq_url(mock_blocking_connection, mock_rabbitmq_uri):
     mock_connection = MagicMock()
     mock_channel = MagicMock()
     mock_connection.channel.return_value = mock_channel
@@ -29,6 +29,7 @@ def test_parse_rabbitmq_url(mock_blocking_connection):
 
     mock_blocking_connection.assert_called_once()
 
-    expected_url = os.getenv('RABBITMQ_URL', 'mocked_rabbitmq_url')
+    expected_url = mock_rabbitmq_uri
+    
     if result != expected_url:
         pytest.fail(f"Expected '{expected_url}', but got {result}")
